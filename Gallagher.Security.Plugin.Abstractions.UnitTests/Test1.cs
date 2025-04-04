e515a77fa60526f354e01ecd51f5cf4d6d9996fc
Ricardo Wallace <ricardo.wallace@gallagher.com>
2025-04-04 15:36:36 +1300

    Add Gallagher Security plugin projects and tests
    - Updated binary files: `ModuleA.dll`, `Plugin.Abstractions.dll`, `ModuleB.dll`
    - Modified solution file to include new Gallagher Security projects
    - Created unit test projects targeting `.NET 8.0`
    - Enabled parallel test execution in `MSTestSettings.cs`
    - Added placeholder test classes in `Test1.cs`
    - Created project files for `Gallagher.Security.Plugin.Abstractions` and `Gallagher.Security.Plugin.ModuleLoader` targeting `.NET Standard 2.0`
    - Introduced WPF application structure with `App.xaml` and `MainWindow.xaml`
    - Updated `AssemblyInfo.cs` for WPF theme information

---
+++ Gallagher.Security.Plugin.Abstractions.UnitTests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Gallagher.Security.Plugin.Abstractions.UnitTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Gallagher.Security.Plugin.C3Simulator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Gallagher.Security.Plugin.C3Simulator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
